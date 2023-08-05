6fb327627cf4bd53171e3b0721f937e70d3e43b7
Leandro Echegorria <leandro.echegorria@gmail.com>
2023-08-05 19:05:51 -0300

    nodemon + express

--- Desafio3/src/mainD3.js
+++ Desafio3/src/mainD3.js
@@ -2,9 +2,9 @@ import http from 'http'
 
 const PORT = 5000
 const server = http.createServer((req, res) => {
-    res.end("Mi primer servidor desde back")
+    res.end("Mi primer servidor  back")
 })
 
 server.listen(PORT, ()=> {
-    console.log(`Escuchando desde el puerto ${PORT} `)
+    console.log(`Server on port ${PORT} `)
 })
