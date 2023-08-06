01692cc54347270599327a4a22b712bb21e29217
Leandro Echegorria <leandro.echegorria@gmail.com>
2023-08-06 15:29:56 -0300

    Express Server

--- Desafio3/src/mainD3.js
+++ Desafio3/src/mainD3.js
@@ -1,4 +1,4 @@
-import http from 'http'
+/* import http from 'http'
 
 const PORT = 5000
 const server = http.createServer((req, res) => {
@@ -7,4 +7,18 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, ()=> {
     console.log(`Server on port ${PORT} `)
+}) */
+
+
+import express from 'express'
+
+const app = express()
+const PORT = 5000
+
+app.get('/', (req, res) => {
+    res.send("Hola desde la pag de inicio de la app ssss")
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server express on port ${PORT}`)
 })
